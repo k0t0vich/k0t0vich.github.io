266b19e74a26fd965dff54decb1f6d24a52daf35
k.bondarenko <k.bondarenko@corp.mail.ru>
2018-07-06 13:28:12 +0300

    vue block test add src

---
+++ vue/ block-test/src/components/Block.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="block">
+    <ol>
+      <li v-for="todo in todos" :style="st">
+        {{ todo.text }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "Block",
+      data() {
+          return {
+            todos: [
+              { text: '*' },
+              { text: '*' },
+              { text: '*' },
+              { text: '*' },
+              { text: '*' },
+              { text: '*' },
+              { text: '*' }
+            ],
+           st: "background-color:#7f7f7f"
+          }
+      },
+      props: ['msg']
+    }
+</script>
+
+<style scoped>
+  li {
+    border: #000066;
+  }
+</style>
---
+++ vue/ block-test/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<!-- eslint-disable -->
+<template>
+  <div class="hello">
+    <Block ref="one"/>
+    <button @click="clickHandler">{{rev ? "<":">"}}</button>
+    <Block ref="two"/>
+  </div>
+</template>
+
+<script>
+  import Block from './Block';
+
+  export default {
+    name: 'HelloWorld',
+    components: {
+      Block
+    },
+    data () {
+      return {
+        rev:false
+      }
+    },
+    methods: {
+      clickHandler: function () {
+        var oneTodos = this.$refs.one.todos;
+        var twoTodos = this.$refs.two.todos;
+
+        if (oneTodos.length == 0 || twoTodos.length == 0)
+          this.rev = !this.rev;
+
+        if (this.rev)
+          oneTodos.push(twoTodos.pop());
+        else
+          twoTodos.push(oneTodos.pop());
+
+        this.$refs.one.st =  "background-color:" + getRandomColorString();
+        this.$refs.two.st =  "background-color:" + getRandomColorString();
+      }
+    }
+  }
+
+  var getRandomColorString = () => ("#" + Math.floor(Math.random() * 255 * 255 * 255).toString(16));
+
+</script>
+
+<style scoped>
+</style>
