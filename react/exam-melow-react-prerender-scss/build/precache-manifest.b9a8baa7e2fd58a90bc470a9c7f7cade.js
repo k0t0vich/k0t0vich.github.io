8b39f0aae5e509edb68fa1df8c4ac1a339c8e87d
Konstantin Bondarenko <b_konstantin@list.ru>
2019-08-14 13:41:31 +0300

    optimized resources

---
+++ react/exam-melow-react-prerender-scss/build/precache-manifest.b9a8baa7e2fd58a90bc470a9c7f7cade.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3965ea986c044b2f63ca453dff3427e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8640beec579ae9de48c0",
+    "url": "/static/css/main.bfd0bf9e.chunk.css"
+  },
+  {
+    "revision": "3d83a6d65d2be3861daf",
+    "url": "/static/js/2.f9fe273c.chunk.js"
+  },
+  {
+    "revision": "8640beec579ae9de48c0",
+    "url": "/static/js/main.84705d04.chunk.js"
+  },
+  {
+    "revision": "3a1253f9dfbb66cc9697",
+    "url": "/static/js/runtime~main.a13f104f.js"
+  }
+]);
