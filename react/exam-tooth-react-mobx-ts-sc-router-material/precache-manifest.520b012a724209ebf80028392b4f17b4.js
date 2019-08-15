80bd5240a6d5116f1173e5426d03ad7a3b01dd5f
Konstantin Bondarenko <b_konstantin@list.ru>
2019-08-15 23:02:41 +0300

    Add files via upload

---
+++ react/exam-tooth-react-mobx-ts-sc-router-material/precache-manifest.520b012a724209ebf80028392b4f17b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c563b2c171dca8a655faee5f502cd2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9be12d6deb51b22ddb3a",
+    "url": "./static/css/main.d0810cdb.chunk.css"
+  },
+  {
+    "revision": "83062f694716eae8d949",
+    "url": "./static/js/2.0ae8075b.chunk.js"
+  },
+  {
+    "revision": "9be12d6deb51b22ddb3a",
+    "url": "./static/js/main.e1697d4e.chunk.js"
+  },
+  {
+    "revision": "063d7f4b331d4e688315",
+    "url": "./static/js/runtime~main.f7265b1c.js"
+  }
+]);
--- react/exam-tooth-react-mobx-ts-sc-router-material/service-worker.js
+++ react/exam-tooth-react-mobx-ts-sc-router-material/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da08d325c3d10e9932faa5d1a2db2150.js"
+  "./precache-manifest.520b012a724209ebf80028392b4f17b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
