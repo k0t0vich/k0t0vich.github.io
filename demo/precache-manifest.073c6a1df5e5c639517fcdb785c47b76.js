c4dcdee954af0bc944379c5e4feed76febeadf7c
Konstantin Bondarenko <b_konstantin@list.ru>
2020-11-07 08:42:05 +0300

    added threejs demo

---
+++ demo/precache-manifest.073c6a1df5e5c639517fcdb785c47b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8657de0bfa53ed3c2563d3aa03f407e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "028fad22ab5420bc43cf",
+    "url": "/static/js/2.4253d9b9.chunk.js"
+  },
+  {
+    "revision": "3e5acc9c12d932e36cd68b347a60f6e7",
+    "url": "/static/js/2.4253d9b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd3893677a45b8087f1",
+    "url": "/static/js/main.2c5bbb9e.chunk.js"
+  },
+  {
+    "revision": "11526822b48deaff34f9",
+    "url": "/static/js/runtime-main.0fe6f316.js"
+  }
+]);
